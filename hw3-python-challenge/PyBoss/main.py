34857e10826727f106e8dfca393c93858641a010
angang-li <angang-li@hotmail.com>
2018-04-29 08:45:41 -0500

    Read and retrieve employee record

---
+++ hw3-python-challenge/PyBoss/main.py
@@ -0,0 +1,35 @@
+# Convert employee record into a different format
+# 2018-04-29
+
+import os
+import csv
+import us_state_abbrev
+
+# Specify file path
+directory = 'raw_data'
+file_name = 'employee_data1.csv'
+file_path = os.path.join(directory, file_name)
+
+# Initiate variables
+dictionary = {}
+new_data_list = []
+fieldnames = ['Emp ID', '']
+
+# Retrieve data from csv data
+with open(file_path, newline = '') as file_in:
+    reader = csv.DictReader(file_in, delimiter = ',')
+    for row in reader:
+        dictionary['Emp ID'] = row['Emp ID']
+        first, last = row['Name'].split(' ')
+        dictionary['First Name'] = first
+        dictionary['Last Name'] = last
+        year, month, day = row['DOB'].split('-')
+        dictionary['DOB'] = '/'.join([month, day, year])
+        ssn_last = row['SSN'].split('-')[2]
+        ssn_hidden = '***-**-' + ssn_last
+        dictionary['SSN'] = ssn_hidden
+        state_abbrev = us_state_abbrev.us_state_abbrev[row['State']]
+        dictionary['State'] = state_abbrev
+        new_data_list.append(dictionary)
+
+
