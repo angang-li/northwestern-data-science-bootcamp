6db592bbbd99e57c2f0e8129fdf45ac5cbfb70b8
angang-li <angang-li@hotmail.com>
2018-04-29 19:18:35 -0500

    Read paragraph into a list of words

--- hw3-python-challenge/PyParagraph/main.py
+++ hw3-python-challenge/PyParagraph/main.py
@@ -2,6 +2,7 @@
 # 2018-04-29
 
 import os
+import re
 
 # Specify file path
 directory = 'raw_data'
@@ -11,4 +12,12 @@ file_path = os.path.join(directory, file_name)
 # Read the paragraph into a string
 with open(file_path, 'r') as file_in:
     paragraph = file_in.read()
-    
+
+# Read paragraph into a list of words
+words_rough = paragraph.strip().split(' ')
+words_clean = []
+for word_rough in words_rough:
+    word_clean = word_rough.strip().strip('(),.!?;:{}[]><').lower()
+    if len(word_clean) > 0:
+        words_clean.append(word_clean)
+
